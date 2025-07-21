82b475172fcd820c2ecd2f3c043df7f9fabc1e1b
Harini Rajendran <harinirajendran1102@gmail,com>
2025-07-21 15:03:28 +0530

    Fix: Compile model before training

--- utils.py
+++ utils.py
@@ -1,42 +1,34 @@
-import matplotlib.pyplot as plt
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.optimizers import Adam
-
-def create_model(use_dropout, dropout_rate):
-    model = Sequential()
-    model.add(Dense(512, activation="relu", input_shape=(784,)))
-    if use_dropout:
-        model.add(Dropout(dropout_rate))
-    model.add(Dense(256, activation="relu"))
-    if use_dropout:
-        model.add(Dropout(dropout_rate))
-    model.add(Dense(10, activation="softmax"))
-    model.compile(loss="categorical_crossentropy", optimizer=Adam(), metrics=["accuracy"])
-    return model
+import matplotlib.pyplot as plt
 
 def train_and_evaluate(model, x_train, y_train, x_test, y_test, epochs, batch_size):
+    model.compile(
+        loss='categorical_crossentropy',
+        optimizer=Adam(),
+        metrics=['accuracy']
+    )
+
     history = model.fit(
         x_train, y_train,
-        validation_data=(x_test, y_test),
+        validation_split=0.2,
         epochs=epochs,
         batch_size=batch_size,
         verbose=0
     )
+
     test_loss, test_acc = model.evaluate(x_test, y_test, verbose=0)
     return history, test_acc
 
-
 def plot_training(history):
     fig, ax = plt.subplots(1, 2, figsize=(12, 4))
-    
+
     ax[0].plot(history.history["loss"], label="Train Loss")
     ax[0].plot(history.history["val_loss"], label="Val Loss")
     ax[0].set_title("Loss")
     ax[0].legend()
 
-    ax[1].plot(history.history["accuracy"], label="Train Acc")
-    ax[1].plot(history.history["val_accuracy"], label="Val Acc")
+    ax[1].plot(history.history["accuracy"], label="Train Accuracy")
+    ax[1].plot(history.history["val_accuracy"], label="Validation Accuracy")
     ax[1].set_title("Accuracy")
     ax[1].legend()
 
